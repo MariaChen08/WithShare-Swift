02471b1f359072fd7a3158cc040434037d499839
MariaChen08 <MariaChen@client-104-39-124-17.mobility.psu.edu>
2016-06-28 22:45:24 -0400

    upload profile image

--- WithShare-Swift/UploadPhotoViewController.swift
+++ WithShare-Swift/UploadPhotoViewController.swift
@@ -8,6 +8,41 @@
 
 import UIKit
 
-class UploadPhotoViewController: UIViewController {
+class UploadPhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    var imagePicker: UIImagePickerController!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        imagePicker.delegate = self
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func choosePhoto(sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
 
+    @IBAction func takePhoto(sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        profileImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
 }
