b438c05bd586799318ae3f75408ece5b2755854f
MariaChen08 <MariaChen@client-104-39-124-17.mobility.psu.edu>
2016-06-29 15:13:13 -0400

    show popover menu items

--- WithShare-Swift/ActivityTypePopoverMenuViewController.swift
+++ WithShare-Swift/ActivityTypePopoverMenuViewController.swift
@@ -8,8 +8,31 @@
 
 import UIKit
 
-class ActivityTypePopoverMenuViewController: UIViewController {
+class ActivityTypePopoverMenuViewController: UITableViewController {
+    
     //MARK: Properties
     
+    let activityTypes = ["All Activities", "Eat Out", "Physical Activities", "Group Study", "Socializing", "More"]
+    
+    let activityTypeCellIdentifier = "activityTypeCell"
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(activityTypes.count)
+        return activityTypes.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(activityTypeCellIdentifier, forIndexPath: indexPath) 
+        cell.textLabel?.text = activityTypes[indexPath.row]
+        return cell
+    }
 
 }
