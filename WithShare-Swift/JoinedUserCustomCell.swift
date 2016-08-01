7779a8c2a6019e72c83cbb7d3c637048d0d1de57
MariaChen08 <jwchen.maria@gmail.com>
2016-07-31 23:01:33 -0400

    detail of my post

---
+++ WithShare-Swift/JoinedUserCustomCell.swift
@@ -0,0 +1,20 @@
+//
+//  JoinedUserCustomCell.swift
+//  WithShare-Swift
+//
+//  Created by Jiawei Chen on 7/31/16.
+//  Copyright © 2016 Jiawei Chen. All rights reserved.
+//
+
+import UIKit
+
+class JoinedUserCustomCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var departmentLabel: UILabel!
+    @IBOutlet weak var gradeLabel: UILabel!
+    @IBOutlet weak var hobbyLabel: UILabel!
+    
+    @IBOutlet weak var joinTimeLabel: UILabel!
+}
---
+++ WithShare-Swift/MyActivityDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MyActivityDetailViewController.swift
+//  WithShare-Swift
+//
+//  Created by Jiawei Chen on 7/31/16.
+//  Copyright © 2016 Jiawei Chen. All rights reserved.
+//
+
+import UIKit
+
+class MyActivityDetailViewController: UIViewController {
+
+}
